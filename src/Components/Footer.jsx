56a7d44790d4bd00aa7adf0329626ac7a6b266c7
tareq179 <abdulkarimctg373@gmail.com>
2021-09-26 22:11:42 +0600

    coplet

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,6 +12,13 @@ import {
 const Container = styled.div `
     display: flex;
 `
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
 const Logo = styled.h1 ``
 
 const Desc = styled.p `
@@ -21,21 +28,54 @@ const Desc = styled.p `
 const SocialContainer = styled.div `
     display: flex;
 `
-const SocialIcon = styled.div `
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    color: white;
-    background-color: #${(props) => props.color};
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+`;
+
+const Center = styled.div `
+    flex: 1;
+    padding: 20px;
+`
+const Title = styled.h3 `
+    margin: 30px;
+`
+const List = styled.ul `
+    margin: 0;
+    padding: 0;
+    list-style: none;
     display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-right: 20px;
+    flex-wrap: wrap;
+`
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+const Right = styled.div `
+    flex: 1;
+    padding: 20px; 
 `
 
-const Left = styled.div ``
-const Center = styled.div ``
-const Right = styled.div ``
+const ContactItem = styled.div `
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`
+
+const Payment = styled.img `
+    width: 50%;
+    cursor: pointer;
+`
 
 const Footer = () => {
     return (
@@ -63,8 +103,36 @@ const Footer = () => {
             </SocialIcon>
           </SocialContainer>
         </Left>
-        <Center></Center>
-        <Right></Right>
+        <Center>
+          <Title>Useful Links</Title>
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Cart </ListItem>
+            <ListItem>Man Fashion</ListItem>
+            <ListItem>Woman Fashion</ListItem>
+            <ListItem>Accessories</ListItem>
+            <ListItem>My Account</ListItem>
+            <ListItem>Order Tracking</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Terms</ListItem>
+          </List>
+        </Center>
+        <Right>
+          <Title>Contact</Title>
+          <ContactItem>
+            <Room style={{ marginRight: "10px" }} />
+            Patiya, Chattogram.
+          </ContactItem>
+          <ContactItem>
+            <Phone style={{ marginRight: "10px" }} />
+            +1 234 56 78
+          </ContactItem>
+          <ContactItem>
+            <MailOutline style={{ marginRight: "10px" }} />
+            contact@lama.dev
+          </ContactItem>
+          <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+        </Right>
       </Container>
     );
 }
