76de7519da36b6433ad1969a829371c6b2dde702
tareq179 <abdulkarimctg373@gmail.com>
2021-09-27 21:38:12 +0600

    add register and login page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -19,6 +21,12 @@ function App() {
         </Route>
         <Route path="/product">
           <Product />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
         </Route>
       </Switch>
     </Router>
